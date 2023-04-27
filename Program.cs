3df7478ad804a3c2280992081367cec6be8cf3c9
Rokas Braidokas <rokasbraidokas@gmail.com>
2023-04-27 14:43:42 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string filePath = @"./../../U3.txt";
+            Sudoku sudoku = new Sudoku();
+            sudoku.Read(filePath);
+            sudoku.Solve();
+            Console.WriteLine(sudoku.Answer());
+        }
+    }
+    class Sudoku
+    {
+        private int[,] lenta;
+        public Sudoku()
+        {
+            lenta = new int[6, 6];
+        }
+        public void Solve()
+        {
+
+        }
+        public void Read(string filePath)
+        {
+            
+        }
+        public string Answer()
+        {
+            return "";
+        }
+    }
+}
