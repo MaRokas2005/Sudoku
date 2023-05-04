a4f26d4bb9618b7f33a8e0fa4e6db731fdd5341f
Rokas Braidokas <rokasbraidokas@gmail.com>
2023-05-04 14:06:49 +0300

    Finished

--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Sudoku
 {
@@ -22,31 +21,147 @@ namespace Sudoku
     class Sudoku
     {
         private int[,] lenta;
+        private int[,] answer;
         public Sudoku()
         {
             lenta = new int[6, 6];
         }
         public void Solve()
         {
-
+            List<(int x, int y)> nuliai = Zeroes(lenta);
+            List<List<int>> skaiciai = new List<List<int>>();
+            GenerateNumbers(nuliai.Count(), new List<int>(), 0, skaiciai);
+            List<int[,]> lentos = Join(lenta, nuliai, skaiciai);
+            Check(lentos);
         }
         public void Read(string filePath)
         {
             string[] lines = File.ReadAllLines(filePath);
-            int a = 0;
+            int eil = 0;
             foreach (string line in lines)
             {
                 string[] nums = line.Trim().Split();
-                for (int i = 0; i < 6; i++)
+                for (int stul = 0; stul < 6; stul++)
                 {
-                    lenta[a, i] = int.Parse(nums[i]);
+                    lenta[eil, stul] = int.Parse(nums[stul]);
                 }
-                a++;
+                eil++;
             }
         }
         public string Answer()
         {
-            return "";
+            if (answer == null)
+            {
+                return "Nėra galimo atsakymo!";
+            }
+            string s = "";
+            for(int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 6; j++)
+                {
+                    s += answer[i, j] + " ";
+                }
+                s += '\n';
+            }
+            return s;
+        }
+        private List<int[,]> Join(int[,] lenta, List<(int x, int y)> nuliai, List<List<int>> skaiciai)
+        {
+            List<int[,]> lentos = new List<int[,]>();
+
+            foreach(var skaic in skaiciai)
+            {
+                int[,] tempLenta = DeepCopy(lenta);
+                for(int i = 0; i < nuliai.Count(); i++)
+                {
+                    int x = nuliai[i].x;
+                    int y = nuliai[i].y;
+                    tempLenta[x, y] = skaic[i];
+                }
+                lentos.Add(tempLenta);
+            }
+            return lentos;
+        }
+        private void GenerateNumbers(int n, List<int> currentNumbers, int index, List<List<int>> numbers)
+        {
+            if (index == n)
+            {
+                numbers.Add(new List<int>(currentNumbers));
+                return;
+            }
+            for(int i = 1; i <= 6; i++)
+            {
+                currentNumbers.Add(i);
+                GenerateNumbers(n, currentNumbers, index + 1, numbers);
+                currentNumbers.RemoveAt(currentNumbers.Count - 1);
+            }
+            return;
+        }
+        private static int[,] DeepCopy(int[,] array)
+        {
+            int width = array.GetLength(0);
+            int height = array.GetLength(1);
+            int[,] copy = new int[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    copy[i, j] = array[i, j];
+                }
+            }
+
+            return copy;
+        }
+        private List<(int x, int y)> Zeroes(int[,] lenta)
+        {
+            List<(int x, int y)> nuliai = new List<(int x, int y)>();
+            for(int eil = 0; eil < 6; eil++)
+            {
+                for(int stul = 0; stul < 6; stul++)
+                {
+                    if (lenta[eil, stul] == 0) nuliai.Add((eil, stul));
+                }
+            }
+            return nuliai;
+        }
+        private bool Check(int[,] lenta)
+        {
+            int[] eilutes    = new int[6];
+            int[] stulpeliai = new int[6];
+            int[] dezutes = new int[6];
+            for(int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 6; j++)
+                {
+                    if (lenta[i, j] == 0) return false;
+                    #region checkSum
+                    int a = i;
+                    int b = j;
+                    int c = i / 2 * 2 + j / 3;
+                    eilutes[a] += (lenta[i, j]);
+                    stulpeliai[b] += (lenta[i, j]);
+                    dezutes[c] += (lenta[i, j]);
+                    #endregion
+                }
+            }
+            for(int i = 0; i < 6; i++)
+            {
+                if (eilutes[i] != 21) return false;
+                if (stulpeliai[i] != 21) return false;
+                if (dezutes[i] != 21) return false;
+            }
+            return true;
+        }
+        private void Check(List<int[,]> lentos)
+        {
+            foreach (var lenta in lentos)
+            {
+                if (Check(lenta)) { 
+                    answer = lenta;
+                    return; 
+                }
+            }
         }
     }
 }
